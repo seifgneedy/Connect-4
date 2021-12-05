3aa44cb61aa4a7832007e641c9013afe083c10a9
abdofawzy841 <58747792+abdofawzy841@users.noreply.github.com>
2021-12-06 01:56:43 +0200

    Update Heuristic.java

--- Util/Heuristic.java
+++ Util/Heuristic.java
@@ -1,15 +1,217 @@
 package Util;
+
+
 public class Heuristic {
-    public int eval(char[][] state){
-        //TODO : Kassem
-        return 0;
+	 final int LENGTH=6;
+	 final int WIDTH=7;
+    public int eval(char[][] board){
+      int util=0;
+      util+=(getAIScore(board)+NumOfConnect_4(board, true))*10000;
+      util-=(getPlayerSore(board)+NumOfConnect_4(board, false))*10000;
+      util+=(getAIScore(board)+NumOfConnect_3(board, true))*100;
+      util-=(getPlayerSore(board)+NumOfConnect_3(board, false))*100;
+      util+=(getAIScore(board)+NumOfConnect_2(board, true))*10;
+      util-=(getPlayerSore(board)+NumOfConnect_2(board, false))*10;
+      
+        return util;
     }
-    int getAIScore(){
-        //TODO: lksjdflkj
-        return 0;
+    int getAIScore(char[][] board){
+   
+       return NumOfConnect_4(board, true);
     }
-    int getPlayerScore(){
-        //TODO: lksdjfl
-        return 0;
+    int getPlayerSore(char[][] board){
+        
+        return NumOfConnect_4(board, false);
+    }
+    
+    private int NumOfConnect_4(char[][] board, boolean AI) {
+    	int num=0;
+    	char piece;
+    	if(AI) piece='a';
+    	else piece='p';
+    	
+    	
+    	 //horizontal
+        for(int i=0;i<LENGTH;i++)
+            for(int j=0;j<WIDTH-3;j++)
+                    if(board[i][j]==piece && board[i][j+1]==piece &&board[i][j+2]==piece &&board[i][j+3]==piece ) num++;
+        
+        //vertical
+        for(int i=0;i<LENGTH-3;i++)
+            for(int j=0;j<WIDTH;j++)
+                    if(board[i][j]==piece && board[i+1][j]==piece &&board[i+2][j]==piece &&board[i+3][j]==piece ) num++;
+        
+        //negative diagonal
+        for(int i=3;i<LENGTH;i++)
+            for(int j=0;j<WIDTH-3;j++)
+                    if(board[i][j]==piece && board[i-1][j+1]==piece &&board[i-2][j+2]==piece &&board[i-3][j+3]==piece ) num++;
+        
+        //positive diagonal
+        for(int i=0;i<LENGTH-3;i++)
+            for(int j=0;j<WIDTH-3;j++)
+                    if(board[i][j]==piece && board[i+1][j+1]==piece &&board[i+2][j+2]==piece &&board[i+3][j+3]==piece ) num++;
+
+    	
+    	return num; 
+    }
+    
+    private int NumOfConnect_3(char[][] board, boolean AI) {
+    	int num=0;
+    	char piece;
+    	if(AI) piece='a';
+    	else piece='p';
+    
+    	 for(int i=0;i<LENGTH;i++)
+             for(int j=0;j<WIDTH-3;j++)
+                     if(board[i][j]=='_' && board[i][j+1]==piece &&board[i][j+2]==piece&&board[i][j+3]==piece ) num++;
+         
+         for(int i=0;i<LENGTH;i++)
+             for(int j=0;j<WIDTH-3;j++)
+                     if(board[i][j]==piece && board[i][j+1]=='_' &&board[i][j+2]==piece&&board[i][j+3]==piece ) num++;
+         
+         for(int i=0;i<LENGTH;i++)
+             for(int j=0;j<WIDTH-3;j++)
+                     if(board[i][j]==piece && board[i][j+1]==piece &&board[i][j+2]=='_'&&board[i][j+3]==piece ) num++;
+         
+         for(int i=0;i<LENGTH;i++)
+             for(int j=0;j<WIDTH-3;j++)
+                     if(board[i][j]==piece && board[i][j+1]==piece &&board[i][j+2]==piece&&board[i][j+3]=='_' ) num++;
+         
+         //vertical
+         for(int i=0;i<LENGTH-3;i++)
+             for(int j=0;j<WIDTH;j++)
+                     if(board[i][j]=='_' && board[i+1][j]==piece &&board[i+2][j]==piece &&board[i+3][j]==piece ) num++;
+         for(int i=0;i<LENGTH-3;i++)
+             for(int j=0;j<WIDTH;j++)
+                     if(board[i][j]==piece && board[i+1][j]=='_' &&board[i+2][j]==piece &&board[i+3][j]==piece ) num++;
+         for(int i=0;i<LENGTH-3;i++)
+             for(int j=0;j<WIDTH;j++)
+                     if(board[i][j]==piece && board[i+1][j]==piece &&board[i+2][j]=='_' &&board[i+3][j]==piece ) num++;
+         for(int i=0;i<LENGTH-3;i++)
+             for(int j=0;j<WIDTH;j++)
+                     if(board[i][j]==piece && board[i+1][j]==piece &&board[i+2][j]==piece &&board[i+3][j]=='_' ) num++;
+         
+         //negative diagonal
+         for(int i=3;i<LENGTH;i++)
+             for(int j=0;j<WIDTH-3;j++)
+                     if(board[i][j]=='_' && board[i-1][j+1]==piece &&board[i-2][j+2]==piece &&board[i-3][j+3]==piece ) num++;
+         for(int i=3;i<LENGTH;i++)
+             for(int j=0;j<WIDTH-3;j++)
+                     if(board[i][j]==piece && board[i-1][j+1]=='_' &&board[i-2][j+2]==piece &&board[i-3][j+3]==piece ) num++;
+         for(int i=3;i<LENGTH;i++)
+             for(int j=0;j<WIDTH-3;j++)
+                     if(board[i][j]==piece && board[i-1][j+1]==piece &&board[i-2][j+2]=='_' &&board[i-3][j+3]==piece ) num++;
+         for(int i=3;i<LENGTH;i++)
+             for(int j=0;j<WIDTH-3;j++)
+                     if(board[i][j]==piece && board[i-1][j+1]==piece &&board[i-2][j+2]==piece &&board[i-3][j+3]=='_' ) num++;
+         
+         //positive diagonal
+         for(int i=0;i<LENGTH-3;i++)
+             for(int j=0;j<WIDTH-3;j++)
+                     if(board[i][j]=='_' && board[i+1][j+1]==piece &&board[i+2][j+2]==piece &&board[i+3][j+3]==piece ) num++;
+         for(int i=0;i<LENGTH-3;i++)
+             for(int j=0;j<WIDTH-3;j++)
+                     if(board[i][j]==piece && board[i+1][j+1]=='_' &&board[i+2][j+2]==piece &&board[i+3][j+3]==piece ) num++;
+         for(int i=0;i<LENGTH-3;i++)
+             for(int j=0;j<WIDTH-3;j++)
+                     if(board[i][j]==piece && board[i+1][j+1]==piece &&board[i+2][j+2]=='_' &&board[i+3][j+3]==piece ) num++;
+         for(int i=0;i<LENGTH-3;i++)
+             for(int j=0;j<WIDTH-3;j++)
+                     if(board[i][j]==piece && board[i+1][j+1]==piece &&board[i+2][j+2]==piece &&board[i+3][j+3]=='_' ) num++;
+
+    	return num;
+    }
+    
+    private int NumOfConnect_2(char[][] board, boolean AI) {
+    	int num=0;
+    	char piece;
+    	if(AI) piece='a';
+        else piece='p';
+    
+    	//horizontal
+        for(int i=0;i<LENGTH;i++)
+            for(int j=0;j<WIDTH-3;j++)
+                    if(board[i][j]=='_' && board[i][j+1]=='_' &&board[i][j+2]==piece&&board[i][j+3]==piece ) num++;
+        
+        for(int i=0;i<LENGTH;i++)
+            for(int j=0;j<WIDTH-3;j++)
+                    if(board[i][j]=='_' && board[i][j+1]==piece &&board[i][j+2]=='_'&&board[i][j+3]==piece ) num++;
+        
+        for(int i=0;i<LENGTH;i++)
+            for(int j=0;j<WIDTH-3;j++)
+                    if(board[i][j]=='_' && board[i][j+1]==piece &&board[i][j+2]==piece&&board[i][j+3]=='_' ) num++;
+        
+        for(int i=0;i<LENGTH;i++)
+            for(int j=0;j<WIDTH-3;j++)
+                    if(board[i][j]==piece && board[i][j+1]=='_' &&board[i][j+2]==piece&&board[i][j+3]=='_' ) num++;
+        
+        for(int i=0;i<LENGTH;i++)
+            for(int j=0;j<WIDTH-3;j++)
+                    if(board[i][j]==piece && board[i][j+1]=='_' &&board[i][j+2]=='_'&&board[i][j+3]==piece ) num++;
+        
+        for(int i=0;i<LENGTH;i++)
+            for(int j=0;j<WIDTH-3;j++)
+                    if(board[i][j]==piece && board[i][j+1]==piece &&board[i][j+2]=='_'&&board[i][j+3]=='_' ) num++;
+        
+        //vertical
+        for(int i=0;i<LENGTH-3;i++)
+            for(int j=0;j<WIDTH;j++)
+                    if(board[i][j]=='_' && board[i+1][j]=='_' &&board[i+2][j]==piece &&board[i+3][j]==piece ) num++;
+        for(int i=0;i<LENGTH-3;i++)
+            for(int j=0;j<WIDTH;j++)
+                    if(board[i][j]=='_' && board[i+1][j]==piece &&board[i+2][j]=='_' &&board[i+3][j]==piece ) num++;
+        for(int i=0;i<LENGTH-3;i++)
+            for(int j=0;j<WIDTH;j++)
+                    if(board[i][j]=='_' && board[i+1][j]==piece &&board[i+2][j]==piece &&board[i+3][j]=='_' ) num++;
+        for(int i=0;i<LENGTH-3;i++)
+            for(int j=0;j<WIDTH;j++)
+                    if(board[i][j]==piece && board[i+1][j]=='_' &&board[i+2][j]==piece &&board[i+3][j]=='_' ) num++;
+        for(int i=0;i<LENGTH-3;i++)
+            for(int j=0;j<WIDTH;j++)
+                    if(board[i][j]==piece && board[i+1][j]=='_' &&board[i+2][j]=='_' &&board[i+3][j]==piece ) num++;
+        for(int i=0;i<LENGTH-3;i++)
+            for(int j=0;j<WIDTH;j++)
+                    if(board[i][j]==piece && board[i+1][j]==piece &&board[i+2][j]=='_' &&board[i+3][j]=='_' ) num++;
+        
+        //negative diagonal
+        for(int i=3;i<LENGTH;i++)
+            for(int j=0;j<WIDTH-3;j++)
+                    if(board[i][j]=='_' && board[i-1][j+1]=='_' &&board[i-2][j+2]==piece &&board[i-3][j+3]==piece ) num++;
+        for(int i=3;i<LENGTH;i++)
+            for(int j=0;j<WIDTH-3;j++)
+                    if(board[i][j]=='_' && board[i-1][j+1]==piece &&board[i-2][j+2]=='_' &&board[i-3][j+3]==piece ) num++;
+        for(int i=3;i<LENGTH;i++)
+            for(int j=0;j<WIDTH-3;j++)
+                    if(board[i][j]=='_' && board[i-1][j+1]==piece &&board[i-2][j+2]==piece &&board[i-3][j+3]=='_' ) num++;
+        for(int i=3;i<LENGTH;i++)
+            for(int j=0;j<WIDTH-3;j++)
+                    if(board[i][j]==piece && board[i-1][j+1]=='_' &&board[i-2][j+2]==piece &&board[i-3][j+3]=='_' ) num++;
+        for(int i=3;i<LENGTH;i++)
+            for(int j=0;j<WIDTH-3;j++)
+                    if(board[i][j]==piece && board[i-1][j+1]=='_' &&board[i-2][j+2]=='_' &&board[i-3][j+3]==piece ) num++;
+        for(int i=3;i<LENGTH;i++)
+            for(int j=0;j<WIDTH-3;j++)
+                    if(board[i][j]==piece && board[i-1][j+1]==piece &&board[i-2][j+2]=='_' &&board[i-3][j+3]=='_' ) num++;
+        
+        //positive diagonal
+        for(int i=0;i<LENGTH-3;i++)
+            for(int j=0;j<WIDTH-3;j++)
+                    if(board[i][j]=='_' && board[i+1][j+1]=='_' &&board[i+2][j+2]==piece &&board[i+3][j+3]==piece ) num++;
+        for(int i=0;i<LENGTH-3;i++)
+            for(int j=0;j<WIDTH-3;j++)
+                    if(board[i][j]=='_' && board[i+1][j+1]==piece &&board[i+2][j+2]=='_' &&board[i+3][j+3]==piece ) num++;
+        for(int i=0;i<LENGTH-3;i++)
+            for(int j=0;j<WIDTH-3;j++)
+                    if(board[i][j]=='_' && board[i+1][j+1]==piece &&board[i+2][j+2]==piece &&board[i+3][j+3]=='_' ) num++;
+        for(int i=0;i<LENGTH-3;i++)
+            for(int j=0;j<WIDTH-3;j++)
+                    if(board[i][j]==piece && board[i+1][j+1]=='_' &&board[i+2][j+2]==piece &&board[i+3][j+3]=='_' ) num++;
+        for(int i=0;i<LENGTH-3;i++)
+            for(int j=0;j<WIDTH-3;j++)
+                    if(board[i][j]==piece && board[i+1][j+1]=='_' &&board[i+2][j+2]=='_' &&board[i+3][j+3]==piece ) num++;
+        for(int i=0;i<LENGTH-3;i++)
+            for(int j=0;j<WIDTH-3;j++)
+                    if(board[i][j]==piece && board[i+1][j+1]==piece &&board[i+2][j+2]=='_' &&board[i+3][j+3]=='_' ) num++;
+    return num;
     }
 }
